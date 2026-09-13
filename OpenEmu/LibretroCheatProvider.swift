35fd49512804557cd5431b1a99f4cdeddc52ec0d
Leonardo Kasperavičius <leocck@gmail.com>
2026-09-13 21:44:27 +0200

    Add Atari 7600 system to the Libretro Database integration

--- OpenEmu/LibretroCheatProvider.swift
+++ OpenEmu/LibretroCheatProvider.swift
@@ -71,6 +71,7 @@ final class LibretroCheatProvider: CheatDatabaseProvider, @unchecked Sendable {
     private let systemMap: [String: String] = [
         OESystemIdentifierAtari2600: "Atari - 2600",
         OESystemIdentifier5200:      "Atari - 5200",
+        OESystemIdentifier7800:      "Atari - 7800",
         OESystemIdentifierSMS:       "Sega - Master System - Mark III",
         OESystemIdentifierNES:       "Nintendo - Nintendo Entertainment System",
         OESystemIdentifierFDS:       "Nintendo - Family Computer Disk System",
